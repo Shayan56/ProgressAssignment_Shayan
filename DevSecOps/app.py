24cb544dec006416e12ae6ad9667673451e0dd22
Shayan56 <shayana.btech22@rvu.edu.in>
2025-09-26 21:09:32 +0530

    Fix flake8 blank line errors

--- DevSecOps/app.py
+++ DevSecOps/app.py
@@ -1,6 +1,6 @@
-# app.py
 def greet(name):
     return f"Hello, {name}!"
+
 
 if __name__ == "__main__":
     name = input("Enter your name: ")
--- DevSecOps/test_app.py
+++ DevSecOps/test_app.py
@@ -3,3 +3,6 @@ from app import greet
 
 def test_greet():
     assert greet("Shayan") == "Hello, Shayan!"
+
+
+# If you add more tests, they should start after 2 blank lines
